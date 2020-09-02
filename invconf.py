b800c93a39073aba3726d9f86edcbe3feb09e717
Andriy Shapochka <ashapochka@gmail.com>
2020-09-02 15:59:46 +0300

    commands supporting webapp creation and configuration

--- invconf.py
+++ invconf.py
@@ -1,4 +1,7 @@
 # cross resource
+subscription = '5a1f3d2e-15e3-4ace-a76c-73b7c1df42ca'
+company_name = 'softserveinc'
+tenant_prefix = f'{company_name}-demo'
 app_name = "chainvoice"
 
 tags = {
@@ -24,4 +27,13 @@ image = {
     'name': app_name,
     'acr_name': f"{cr['login_server']}/{app_name}",
     'acr_latest': f"{cr['login_server']}/{app_name}:latest"
+}
+
+# app service
+# webapp_principal is dynamic, from task webapp_identity_assign
+appservice = {
+    'plan_name': f"{app_name}-plan",
+    'sku': 'B1',
+    'webapp_name': f'{tenant_prefix}-{app_name}',
+    'webapp_principal': '614a96a5-2ea4-4675-91c7-dc1552196cc9'
 }
--- tasks/admin_azure.py
+++ tasks/admin_azure.py
@@ -28,6 +28,7 @@ def acr_create(c):
               f'--resource-group {c.config.rg.name} ' \
               f'--name {c.config.cr.name} ' \
               f'--sku {c.config.cr.sku} ' \
+              f'--admin-enabled true ' \
               f'--tags {c.config.tags.owner}'
     run_command(command, c, logger)
 
@@ -53,4 +54,108 @@ def acr_tags(c):
               f'--name {c.config.cr.name} ' \
               f'--repository {c.config.image.name} ' \
               f'--output table'
+    run_command(command, c, logger)
+
+
+@task
+def appservice_create_plan(c):
+    command = f'az appservice plan create ' \
+              f'--name {c.config.appservice.plan_name} ' \
+              f'--resource-group {c.config.rg.name} ' \
+              f'--sku {c.config.appservice.sku} ' \
+              f'--is-linux ' \
+              f'--tags {c.config.tags.owner}'
+    run_command(command, c, logger)
+
+
+@task
+def appservice_delete_plan(c):
+    command = f'az appservice plan delete ' \
+              f'--name {c.config.appservice.plan_name} ' \
+              f'--resource-group {c.config.rg.name} ' \
+              f'--yes'
+    run_command(command, c, logger)
+
+
+@task
+def webapp_create(c):
+    command = f'az webapp create ' \
+              f'--resource-group {c.config.rg.name} ' \
+              f'--plan {c.config.appservice.plan_name} ' \
+              f'--name {c.config.appservice.webapp_name} ' \
+              f'--deployment-container-image-name ' \
+              f'{c.config.image.acr_latest} ' \
+              f'--tags {c.config.tags.owner}'
+    run_command(command, c, logger)
+
+
+@task
+def webapp_delete(c):
+    command = f'az webapp delete ' \
+              f'--resource-group {c.config.rg.name} ' \
+              f'--name {c.config.appservice.webapp_name}'
+    run_command(command, c, logger)
+
+
+@task
+def webapp_restart(c):
+    command = f'az webapp restart ' \
+              f'--name {c.config.appservice.webapp_name} ' \
+              f'--resource-group {c.config.rg.name}'
+    run_command(command, c, logger)
+
+
+@task
+def webapp_log_on(c):
+    command = f'az webapp log config ' \
+              f'--name {c.config.appservice.webapp_name} ' \
+              f'--resource-group {c.config.rg.name} ' \
+              f'--docker-container-logging filesystem'
+    run_command(command, c, logger)
+
+
+@task
+def webapp_log_stream(c):
+    command = f'az webapp log tail ' \
+              f'--name {c.config.appservice.webapp_name} ' \
+              f'--resource-group {c.config.rg.name}'
+    run_command(command, c, logger)
+
+
+@task
+def webapp_settings_set(c, settings):
+    command = f'az webapp config appsettings set ' \
+              f'--name {c.config.appservice.webapp_name} ' \
+              f'--resource-group {c.config.rg.name} ' \
+              f'--settings {settings}'
+    run_command(command, c, logger)
+
+
+@task
+def webapp_identity_assign(c):
+    command = f'az webapp identity assign ' \
+              f'--name {c.config.appservice.webapp_name} ' \
+              f'--resource-group {c.config.rg.name} ' \
+              f'--query principalId --output tsv'
+    run_command(command, c, logger)
+
+
+@task
+def webapp_perm_cr_pull(c):
+    command = f'az role assignment create ' \
+              f'--assignee {c.config.appservice.webapp_principal} ' \
+              f'--scope /subscriptions/{c.config.subscription}/resourceGroups/' \
+              f'{c.config.rg.name}/providers/Microsoft.ContainerRegistry/' \
+              f'registries/{c.config.cr.name} ' \
+              f'--role "AcrPull"'
+    run_command(command, c, logger)
+
+
+@task
+def webapp_container_set(c):
+    command = f'az webapp config container set ' \
+              f'--name {c.config.appservice.webapp_name} ' \
+              f'--resource-group {c.config.rg.name} ' \
+              f'--docker-custom-image-name {c.config.image.acr_latest} ' \
+              f'--docker-registry-server-url https://{c.config.cr.login_server}'
     run_command(command, c, logger)

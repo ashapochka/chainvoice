cdf44bf077aa132898e5ca76521b475feee238d3
Andriy Shapochka <ashapochka@gmail.com>
2020-09-10 17:58:46 +0300

    generalized api, added user api

--- app/api/party_api.py
+++ app/api/party_api.py
@@ -1,54 +1,21 @@
-from typing import List
-from fastapi import (Depends, APIRouter)
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
-from databases import Database
 
-from ..deps import (get_db, get_current_active_user)
 from ..services import party_service
 from ..schemas import (
     PartyCreate, PartyUpdate, PartyGet,
-    UserInDb
 )
+from .base_api import BaseAPI
 
 router = InferringRouter()
 
 
 @cbv(router)
-class PartyApi:
-    db: Database = Depends(get_db)
-    user: UserInDb = Depends(get_current_active_user)
+class PartyAPI(BaseAPI[PartyGet, PartyCreate, PartyUpdate]):
+    service = party_service
 
-    # noinspection PyTypeChecker
-    @router.get("/")
-    async def get_many(
-            self, offset: int = 0, limit: int = 20
-    ) -> List[PartyGet]:
-        return await party_service.get_many(self.db, self.user, offset, limit)
-
-    @router.get("/{uid}/")
-    async def get_one(
-            self, uid: str
-    ) -> PartyGet:
-        return await party_service.get_one_by_uid(self.db, self.user, uid)
-
-    @router.post("/")
-    async def create_one(
-            self, obj: PartyCreate
-    ) -> PartyGet:
-        result = await party_service.create(self.db, self.user, obj)
-        return {**obj.dict(), **result}
-
-    @router.put("/{uid}/")
-    async def update_one(
-            self, uid: str, obj: PartyUpdate
-    ) -> PartyGet:
-        await party_service.update_by_uid(self.db, self.user, uid, obj)
-        return {**obj.dict(), "uid": uid}
-
-    @router.delete("/{uid}/")
-    async def delete_one(
-            self, uid: str
-    ):
-        await party_service.delete_by_uid(self.db, self.user, uid)
-        return {"message": "Object with uid: {} deleted successfully!".format(uid)}
+    get_many = router.get('/')(BaseAPI.get_many)
+    get_one = router.get("/{uid}/")(BaseAPI.get_one)
+    create_one = router.post("/")(BaseAPI.create_one)
+    update_one = router.put("/{uid}/")(BaseAPI.update_one)
+    delete_one = router.delete("/{uid}/")(BaseAPI.delete_one)
--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,59 +1,21 @@
-from typing import List
-from fastapi import (Depends, APIRouter)
-from ..deps import (get_db, get_current_active_user)
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
+
 from ..services import user_service
 from ..schemas import (
-    UserCreate, UserUpdate, UserGet,
-    UserInDb
+    UserGet, UserCreate, UserUpdate,
 )
+from .base_api import BaseAPI
 
-router = APIRouter()
-
-
-# noinspection PyTypeChecker
-@router.get("/", response_model=List[UserGet])
-async def get_many(
-        db=Depends(get_db),
-        user: UserInDb = Depends(get_current_active_user),
-        offset: int = 0, limit: int = 20
-):
-    return await user_service.get_many(db, user, offset, limit)
+router = InferringRouter()
 
 
-@router.get("/{uid}/", response_model=UserGet)
-async def get_one(
-        *, db=Depends(get_db),
-        user: UserInDb = Depends(get_current_active_user),
-        uid: str
-):
-    return await user_service.get_one_by_uid(db, user, uid)
+@cbv(router)
+class UserAPI(BaseAPI[UserGet, UserCreate, UserUpdate]):
+    service = user_service
 
-
-@router.post("/", response_model=UserGet)
-async def create_one(
-        *, db=Depends(get_db),
-        user: UserInDb = Depends(get_current_active_user),
-        obj: UserCreate
-):
-    result = await user_service.create(db, user, obj)
-    return {**obj.dict(), **result}
-
-
-@router.put("/{uid}/", response_model=UserGet)
-async def update_one(
-        *, db=Depends(get_db),
-        user: UserInDb = Depends(get_current_active_user),
-        uid: str, obj: UserUpdate
-):
-    await user_service.update_by_uid(db, user, uid, obj)
-    return {**obj.dict(), "uid": uid}
-
-
-@router.delete("/{uid}/")
-async def delete_one(
-        *, db=Depends(get_db),
-        user: UserInDb = Depends(get_current_active_user),
-        uid: str
-):
-    await user_service.delete_by_uid(db, user, uid)
-    return {"message": "Object with uid: {} deleted successfully!".format(uid)}
+    get_many = router.get('/')(BaseAPI.get_many)
+    get_one = router.get("/{uid}/")(BaseAPI.get_one)
+    create_one = router.post("/")(BaseAPI.create_one)
+    update_one = router.put("/{uid}/")(BaseAPI.update_one)
+    delete_one = router.delete("/{uid}/")(BaseAPI.delete_one)

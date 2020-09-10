d52d80b59d6d3b0f5ca9b5b7d2eb4d187a70f832
Andriy Shapochka <ashapochka@gmail.com>
2020-09-10 19:19:30 +0300

    fixed party and user api to work correctly

--- app/api/base_api.py
+++ app/api/base_api.py
@@ -1,46 +1,10 @@
-from typing import (List, Generic, TypeVar)
 from fastapi import Depends
 from databases import Database
 
 from ..deps import (get_db, get_current_active_user)
 from ..schemas import  UserInDb
 
-SchemaGet = TypeVar('SchemaGet')
-SchemaCreate = TypeVar('SchemaCreate')
-SchemaUpdate = TypeVar('SchemaUpdate')
 
-
-# noinspection PyTypeChecker
-class BaseAPI(Generic[SchemaGet, SchemaCreate, SchemaUpdate]):
+class BaseAPI:
     db: Database = Depends(get_db)
     user: UserInDb = Depends(get_current_active_user)
-    service = None
-
-    async def get_many(
-            self, offset: int = 0, limit: int = 20
-    ) -> List[SchemaGet]:
-        return await self.service.get_many(self.db, self.user, offset, limit)
-
-    async def get_one(
-            self, uid: str
-    ) -> SchemaGet:
-        return await self.service.get_one_by_uid(self.db, self.user, uid)
-
-    async def create_one(
-            self, obj: SchemaCreate
-    ) -> SchemaGet:
-        result = await self.service.create(self.db, self.user, obj)
-        return {**obj.dict(), **result}
-
-    async def update_one(
-            self, uid: str, obj: SchemaUpdate
-    ) -> SchemaGet:
-        await self.service.update_by_uid(self.db, self.user, uid, obj)
-        return {**obj.dict(), "uid": uid}
-
-    async def delete_one(
-            self, uid: str
-    ):
-        await self.service.delete_by_uid(self.db, self.user, uid)
-        return {
-            "message": "Object with uid: {} deleted successfully!".format(uid)}
--- app/api/party_api.py
+++ app/api/party_api.py
@@ -1,5 +1,7 @@
+from typing import List
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
+from fastapi_utils.api_model import APIMessage
 
 from ..services import party_service
 from ..schemas import (
@@ -10,12 +12,42 @@ from .base_api import BaseAPI
 router = InferringRouter()
 
 
+# noinspection PyTypeChecker
 @cbv(router)
-class PartyAPI(BaseAPI[PartyGet, PartyCreate, PartyUpdate]):
+class PartyAPI(BaseAPI):
     service = party_service
 
-    get_many = router.get('/')(BaseAPI.get_many)
-    get_one = router.get("/{uid}/")(BaseAPI.get_one)
-    create_one = router.post("/")(BaseAPI.create_one)
-    update_one = router.put("/{uid}/")(BaseAPI.update_one)
-    delete_one = router.delete("/{uid}/")(BaseAPI.delete_one)
+    @router.get('/')
+    async def get_many(
+            self, offset: int = 0, limit: int = 20
+    ) -> List[PartyGet]:
+        return await self.service.get_many(self.db, self.user, offset, limit)
+
+    @router.get("/{uid}/")
+    async def get_one(
+            self, uid: str
+    ) -> PartyGet:
+        return await self.service.get_one_by_uid(self.db, self.user, uid)
+
+    @router.post("/")
+    async def create_one(
+            self, obj: PartyCreate
+    ) -> PartyGet:
+        result = await self.service.create(self.db, self.user, obj)
+        return {**obj.dict(), **result}
+
+    @router.put("/{uid}/")
+    async def update_one(
+            self, uid: str, obj: PartyUpdate
+    ) -> PartyGet:
+        await self.service.update_by_uid(self.db, self.user, uid, obj)
+        return {**obj.dict(), "uid": uid}
+
+    @router.delete("/{uid}/")
+    async def delete_one(
+            self, uid: str
+    ) -> APIMessage:
+        await self.service.delete_by_uid(self.db, self.user, uid)
+        return APIMessage(
+            detail=f"Object with uid: {uid} deleted successfully!"
+        )

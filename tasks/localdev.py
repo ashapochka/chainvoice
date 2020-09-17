855719cc1b475f58f341a007e6df5a5c93cff620
Andriy Shapochka <ashapochka@gmail.com>
2020-09-17 12:58:26 +0300

    tested deployment of Box contract in azure

--- tasks/localdev.py
+++ tasks/localdev.py
@@ -55,16 +55,37 @@ def export_requirements(c):
 def try_quorum(c):
     from web3 import Web3
     from web3.middleware import geth_poa_middleware
-    import base64
+    import json
 
     url = os.getenv("chainvoice_qnode_url")
     access_key = os.getenv("chainvoice_qnode_key")
     w3 = Web3(Web3.HTTPProvider(f'{url}/{access_key}'))
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     print(f'connected: {w3.isConnected()}')
-    account = w3.eth.account.create()
-    encoded_key = account.key.hex()
-    print(f'address: {account.address}, key: {encoded_key}')
+    with open('build/contracts/Box.json') as f:
+        compiled_contract = json.load(f)
+        box_abi = compiled_contract['abi']
+    contract_address = os.getenv('chainvoice_contract_address_box')
+    account_address = os.getenv('chainvoice_qadmin_address')
+    w3.eth.defaultAccount = account_address
+    account_key = os.getenv('chainvoice_qadmin_private_key')
+    box = w3.eth.contract(
+        address=contract_address,
+        abi=box_abi
+    )
+    contract_call = box.functions.store(11)
+    # print(f'estimated gas: {contract_call.estimateGas()}')
+    # return
+    nonce = w3.eth.getTransactionCount(account_address)
+    txn = contract_call.buildTransaction({
+        'nonce': nonce,
+        'gas': 100000
+    })
+    signed_txn = w3.eth.account.sign_transaction(txn, private_key=account_key)
+    txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    print(f'txn_hash: {txn_hash}')
+    txn_receipt = w3.eth.waitForTransactionReceipt(txn_hash)
+    print(f'txn_receipt: {txn_receipt}')
 
 
 @task

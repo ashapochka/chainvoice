9834f266636492d7da14dd9727ba56e72bc34430
Andriy Shapochka <ashapochka@gmail.com>
2020-09-24 17:50:46 +0300

    added invoice registry proxies to contract

--- app/contracts/invoice_registry_sc.py
+++ app/contracts/invoice_registry_sc.py
@@ -4,16 +4,63 @@ from .base_sc import BaseContract
 
 
 class InvoiceRegistryContract(BaseContract):
-    def safe_transfer_from(
-            self, signer: LocalAccount,
-            from_address: str, to_address:str,
-            token_id: int, amount: int,
-            data: bytes
-    ) -> TxReceipt:
-        contract_call = self.contract.functions.safeTransferFrom(
-            from_address, to_address, token_id, amount, data
-        )
-        return self.send_tx(contract_call, signer)
 
-    def balance_of(self, account: str, token_id: int) -> int:
-        return self.contract.functions.balanceOf(account, token_id).call()
+    def register_invoice(
+            self, seller_account: LocalAccount,
+            invoice_id: bytes, buyer_address:str,
+            token_id: int, amount: int
+    ) -> TxReceipt:
+        """
+        registerInvoice(
+            bytes memory invoiceId,
+            address buyer,
+            uint256 tokenId,
+            uint256 amount
+        )
+        :param seller_account:
+        :param invoice_id:
+        :param buyer_address:
+        :param token_id:
+        :param amount:
+        :return:
+        """
+        contract_call = self.contract.functions.registerInvoice(
+            invoice_id, buyer_address, token_id, amount
+        )
+        return self.send_tx(contract_call, seller_account)
+
+    def publish_invoice(
+            self, seller_account: LocalAccount, invoice_id: bytes
+    ):
+        """
+        publishInvoice(bytes memory invoiceId)
+        :param seller_account:
+        :param invoice_id:
+        :return:
+        """
+        contract_call = self.contract.functions.publishInvoice(
+            invoice_id
+        )
+        return self.send_tx(contract_call, seller_account)
+
+    def cancel_invoice(
+            self, seller_account: LocalAccount, invoice_id: bytes
+    ):
+        """
+        cancelInvoice(bytes memory invoiceId)
+        :param seller_account:
+        :param invoice_id:
+        :return:
+        """
+        contract_call = self.contract.functions.cancelInvoice(
+            invoice_id
+        )
+        return self.send_tx(contract_call, seller_account)
+
+    def get_invoice(self, invoice_id: bytes):
+        """
+        mapping(bytes => Invoice) public invoices;
+        :param invoice_id:
+        :return:
+        """
+        return self.contract.functions.invoices(invoice_id).call()

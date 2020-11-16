46f79caceacaacf9670d7cef65f2e4814220868f
Andriy Shapochka <ashapochka@gmail.com>
2020-11-16 17:10:01 +0200

    dropped unused contracts, added more diagrams

--- contracts/ChainvoiceERC1155.sol
+++ contracts/ChainvoiceERC1155.sol
@@ -8,7 +8,7 @@ contract ChainvoiceERC1155 is ERC1155 {
     uint256 public constant CVT = 0;
 
     constructor() public ERC1155(
-        "https://softserveinc-demo-chainvoice.azurewebsites.net//api/token/{1}.json"
+        "https://softserveinc-demo-chainvoice.azurewebsites.net/api/token/{1}.json"
     ) {
         _mint(msg.sender, CVT, 10**18, "");
     }
